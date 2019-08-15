3d65960aa7dd66cb5ba62532f926cadb44d7101c
aviveershetty <aviveershetty@gmail.com>
2019-08-15 15:14:34 -0400

    added jwt authentication

--- server/handlers/auth.js
+++ server/handlers/auth.js
@@ -1,12 +1,21 @@
 const db = require("../models");
+const jwt = require("jsonwebtoken");
 
 exports.register = async (req, res, next) => {
   try {
     const user = await db.User.create(req.body);
     const { id, username } = user;
-    res.json({ id, username });
+    const token = jwt.sign({ id, username }, process.env.SECRET);
+
+    res.status(201).json({ id, username, token });
   } catch (err) {
-    next(err);
+    if (err.code === 11000) {
+      err.message = "Sorry, that username is already in use";
+    }
+    return next({
+      status: 400,
+      message: err.message
+    });
   }
 };
 
@@ -17,15 +26,13 @@ exports.login = async (req, res, next) => {
     const valid = await user.comparePassword(req.body.password);
 
     if (valid) {
-      //const token = jwt.sign({ id, username }, process.env.SECRET);
-      return res.json({
-        id,
-        username
-      });
+      const token = jwt.sign({ id, username }, process.env.SECRET);
+      return res.json({ id, username, token });
     } else {
-      throw new Error(" Invalid ");
+      throw new Error();
     }
   } catch (err) {
+    err.message = "Invalid user";
     next(err);
   }
 };
--- server/models/index.js
+++ server/models/index.js
@@ -3,16 +3,10 @@ const mongoose = require("mongoose");
 mongoose.set("debug", true);
 
 mongoose.Promise = global.Promise;
-mongoose.connect(
-  "mongodb://localhost/Agasa",
-  {
-    useCreateIndex: true,
-    useNewUrlParser: true
-  },
-  function() {
-    mongoose.connection.db.dropDatabase();
-  }
-);
+mongoose.connect("mongodb://localhost/Agasa", {
+  useCreateIndex: true,
+  useNewUrlParser: true
+});
 
 module.exports.User = require("./user");
 module.exports.Order = require("./order");
